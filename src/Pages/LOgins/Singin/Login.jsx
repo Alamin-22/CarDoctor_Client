67031925b7c3c2e1354255bbd63f403538b5b987
Alamin-22 <mdalaminmollick@gmail.com>
2023-10-30 02:07:43 +0600

    recap token authentication

--- src/Pages/LOgins/Singin/Login.jsx
+++ src/Pages/LOgins/Singin/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
                 const user = { email };
                 // get access token
 
-                axios.post("http://localhost:5000/jwt", user, { withCredentials: true, })
+                axios.post("http://localhost:5000/jwt", user, { withCredentials: true })
                     .then(res => {
                         console.log(res.data)
                         if (res.data.success) {
